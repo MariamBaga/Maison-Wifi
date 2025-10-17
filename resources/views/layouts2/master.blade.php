c51d134ddb74d9d185e54a7d5fb7f8a6bc3448a2
mariam <bagayokom2003@gmail.com>
2025-10-17 18:07:10 +0000

    color master custom

--- resources/views/layouts2/master.blade.php
+++ resources/views/layouts2/master.blade.php
@@ -9,7 +9,7 @@
     @include('layouts2.links')
 </head>
 
-<body class="bg-theme bg-theme11">
+<body class="bg-theme bg-theme-primary">
     <b class="screen-overlay"></b>
     <div class="wrapper">
         {{-- Header --}}
